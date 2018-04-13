e44f31931760f6da789bfb68e15bfcc17fb47068
Jérémy BASTIEN <jeremy.bastien.ext@francetv.fr>
2018-04-13 16:27:51 +0200

    First case: 1 -> I

---
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumerals.swift
@@ -0,0 +1,15 @@
+//
+//  RomanNumerals.swift
+//  RomanNumeralsKata
+//
+//  Created by Jeremy on 13/04/2018.
+//  Copyright © 2018 Jeremy. All rights reserved.
+//
+
+import Foundation
+
+struct RomanNumerals {
+    func convert(number: Int) -> String {
+        return "I"
+    }
+}
---
+++ RomanNumeralsKata/RomanNumeralsKataTests/RomanNumeralsTests.swift
@@ -0,0 +1,33 @@
+//
+//  RomanNumeralsKataTests.swift
+//  RomanNumeralsKataTests
+//
+//  Created by Jeremy on 13/04/2018.
+//  Copyright © 2018 Jeremy. All rights reserved.
+//
+
+import XCTest
+@testable import RomanNumeralsKata
+
+class RomanNumeralsTests: XCTestCase {
+
+    // Subject under test
+    var sut: RomanNumerals!
+    
+    override func setUp() {
+        super.setUp()
+
+        sut = RomanNumerals()
+    }
+
+    // MARK: - Tests
+
+    func testConvert_should_return_I_when_input_is_1() {
+        // When
+        let result = sut.convert(number: 1)
+
+        // Then
+        XCTAssertEqual(result, "I")
+    }
+
+}
