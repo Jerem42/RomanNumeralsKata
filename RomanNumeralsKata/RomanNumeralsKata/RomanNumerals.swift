06ae9e806625413d185d7fd51c61a6c8d02b12bc
Jérémy BASTIEN <jeremy.bastien.ext@francetv.fr>
2018-04-16 11:48:06 +0200

    Removed special cases from Tuple Array

--- RomanNumeralsKata/RomanNumeralsKata/RomanNumerals.swift
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumerals.swift
@@ -14,27 +14,32 @@ struct RomanNumerals {
     var romanAssociationArray: [RomanTuple] =
         [
             ("M", 1000),
-            ("CM", 900),
             ("D", 500),
-            ("CD", 400),
             ("C", 100),
-            ("XC", 90),
             ("L", 50),
-            ("XL", 40),
             ("X", 10),
-            ("IX", 9),
             ("V", 5),
-            ("IV", 4),
             ("I", 1)]
 
     func convert(number: Int) -> String {
         var result = String()
         var remainingNumber = number
 
-        for romanTuple in romanAssociationArray {
-            let repeatedRoman = Int(remainingNumber / romanTuple.numeral)
-            result += String(repeating: romanTuple.roman, count: repeatedRoman)
-            remainingNumber -= (repeatedRoman * romanTuple.numeral)
+        for step in 0..<romanAssociationArray.count {
+            let repeatedRoman = Int(remainingNumber / romanAssociationArray[step].numeral)
+            result += String(repeating: romanAssociationArray[step].roman, count: repeatedRoman)
+            remainingNumber %= romanAssociationArray[step].numeral
+
+            for nextStep in (step + 1)..<romanAssociationArray.count {
+                let stepDiff = romanAssociationArray[step].numeral - romanAssociationArray[nextStep].numeral
+                if stepDiff != romanAssociationArray[nextStep].numeral {
+                    if Int(remainingNumber / stepDiff) == 1 {
+                        result += romanAssociationArray[nextStep].roman + romanAssociationArray[step].roman
+                        remainingNumber %= stepDiff
+                    }
+                    break
+                }
+            }
         }
         return result
     }
